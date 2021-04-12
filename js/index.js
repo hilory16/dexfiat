ff3ca7899e3bea6d3ff192589b928513d200e5a5
Emmanuel Ilori <hiloryolamide@gmail.com>
2021-04-12 02:26:52 +0100

    New fix: AOS, app in dev modal, header links, roadmap update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 
+
+
 $( ".collapse__faq" ).click(function() {
     console.log("object")
     $(this).toggleClass('expand__faq')
 });
+
+$('.app__modal__trigger').click(function(){
+    $(".modal__app").toggleClass('show__modal__app')
+})
+
+$('.app__modal__close').click(function(){
+    $(".modal__app").toggleClass('show__modal__app')
+})
